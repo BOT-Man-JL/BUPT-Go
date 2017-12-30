aa10835be52e7b05061634b04eb35784ba6eae09
BOT Man JL <g123nn@hotmail.com>
2017-12-30 21:14:55 +0800

    Update submit/delete interface;

--- routes/article.js
+++ routes/article.js
@@ -1,8 +1,10 @@
 ﻿'use strict';
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 const mongoClient = require('mongodb').MongoClient;
+const getObjectId = require('mongodb').ObjectID;
 const mongoUrl = 'mongodb://localhost:27017/';
 const dbName = 'mydb';
 const collectionName = 'article';
@@ -35,103 +37,104 @@ mongoClient.connect(mongoUrl, function (err, client) {
 //   - (below article) link to guide
 // - pic
 
-router.get('/', function (req, res) {
-    const _id = req.query.id || 0;
-
-    mongoClient.connect(mongoUrl, function (err, client) {
-        if (err) throw err;
-        const db = client.db(dbName);
-
-        const query = { _id: require('mongodb').ObjectID(_id) };
-        db.collection(collectionName).find(query).toArray(function (err, ret) {
-            if (err) throw err;
-            res.send(ret);
-            console.log('query guide', _id);
-            client.close();
-        });
-    });
-});
-
-router.get('/all-guides', function (req, res) {
-    mongoClient.connect(mongoUrl, function (err, client) {
-        if (err) throw err;
-        const db = client.db(dbName);
-
-        const query = { type: 'guide' };
-        db.collection(collectionName).find(query).toArray(function (err, ret) {
-            if (err) throw err;
-            res.send(ret);
-            console.log('query all-guides');
-            client.close();
-        });
-    });
-});
-
-router.get('/all-places', function (req, res) {
-    mongoClient.connect(mongoUrl, function (err, client) {
-        if (err) throw err;
-        const db = client.db(dbName);
-
-        const query = { type: 'place' };
-        db.collection(collectionName).find(query).toArray(function (err, ret) {
-            if (err) throw err;
-            res.send(ret);
-            console.log('query all-places');
-            client.close();
-        });
-    });
-});
-
-router.get('/new-guide', function (req, res) {
-    mongoClient.connect(mongoUrl, function (err, client) {
-        if (err) throw err;
-        const db = client.db(dbName);
-
-        const newArticle = {
-            type: 'guide', title: 'TITLE', author: 'John', timestamp: new Date()
-        };
-        db.collection(collectionName).insertOne(newArticle, function (err, ret) {
-            if (err) throw err;
-            res.send(ret);
-            console.log('insert', newArticle);
-            client.close();
-        });
-    });
-});
-
-router.get('/new-place', function (req, res) {
-    mongoClient.connect(mongoUrl, function (err, client) {
-        if (err) throw err;
-        const db = client.db(dbName);
-
-        const newArticle = {
-            type: 'place', title: 'TITLE', author: 'Jack', timestamp: new Date()
-        };
-        db.collection(collectionName).insertOne(newArticle, function (err, ret) {
-            if (err) throw err;
-            res.send(ret);
-            console.log('insert', newArticle);
-            client.close();
-        });
-    });
+router.get('/create', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/test-post.html'));
 });
 
 router.get('/edit', function (req, res) {
-    // Note
-    // - use previous article id
-    // - submit or delete
-    res.send('respond with a resource');
+    res.sendFile(path.join(__dirname, '../public/test-post.html'));
+});
+
+router.get('/', function (req, res) {
+    mongoClient.connect(mongoUrl, function (err, client) {
+        if (err) throw err;
+        const db = client.db(dbName);
+
+        const id = req.query.id || 0;
+        const query = { _id: getObjectId(id) };
+        db.collection(collectionName).find(query).toArray(function (err, ret) {
+            if (err) throw err;
+            res.send(ret);
+            console.log('query', id);
+            client.close();
+        });
+    });
+});
+
+router.get('/all', function (req, res) {
+    mongoClient.connect(mongoUrl, function (err, client) {
+        if (err) throw err;
+        const db = client.db(dbName);
+
+        db.collection(collectionName).find().toArray(function (err, ret) {
+            if (err) throw err;
+            res.send(ret);
+            console.log('query all');
+            client.close();
+        });
+    });
 });
 
 router.post('/submit', function (req, res) {
-    res.send('respond with a resource');
+    const type = req.body.type;
+    if (type != 'guide' && type != 'place') {
+        res.send({
+            ok: 0,
+            err: 'bad type'
+        });
+        return;
+    }
+
+    const newArticle = {
+        type: type,
+        title: req.body.title || 'NEW TITLE',
+        author: req.body.author || 'Anonymous',
+        timestamp: new Date()
+    };
+
+    mongoClient.connect(mongoUrl, function (err, client) {
+        if (err) throw err;
+        const db = client.db(dbName);
+
+        const id = req.body.id || null;
+        if (id) {
+            const query = { _id: getObjectId(id) };
+            const article = { $set: newArticle };
+            db.collection(collectionName).updateOne(query, article, function (err, ret) {
+                if (err) throw err;
+                res.send(ret);
+                console.log('update', id, 'to', newArticle);
+                client.close();
+            });
+        }
+        else {
+            db.collection(collectionName).insertOne(newArticle, function (err, ret) {
+                if (err) throw err;
+                res.send(ret);
+                console.log('create', newArticle);
+                client.close();
+            });
+        }
+    });
 });
 
 router.post('/delete', function (req, res) {
-    res.send('respond with a resource');
+    mongoClient.connect(mongoUrl, function (err, client) {
+        if (err) throw err;
+        const db = client.db(dbName);
+
+        const id = req.body.id || 0;
+        const query = { _id: getObjectId(id) };
+        db.collection(collectionName).deleteOne(query, function (err, ret) {
+            if (err) throw err;
+            res.send(ret);
+            console.log('delete', id);
+            client.close();
+        });
+    });
 });
 
-router.get('/upload-pics', function (req, res) {
+router.post('/upload-pics', function (req, res) {
     // Note
     // - write file to mongo
     // - https://stackoverflow.com/a/43592498
