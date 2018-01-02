51face03e11d163b2cb1af3324cfadead4507025
BOT Man JL <g123nn@hotmail.com>
2018-01-02 20:54:29 +0800

    Replace fetch with axios;

--- frontend/src/components/test-ajax.vue
+++ frontend/src/components/test-ajax.vue
@@ -1,14 +1,28 @@
 <template>
     <div class="hello">
-        <p>TODO: add test code here</p>
+        <h3>{{ msg }}</h3>
+        <img v-bind:src="imgSrc" />
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
         name: 'testAjaxPage',
         data() {
-            return null;
+            return {
+                msg: 'loading...',
+                imgSrc: ''
+            };
+        },
+        created() {
+            var vm = this;
+            axios.get('https://yesno.wtf/api').then(function (response) {
+                vm.msg = response.data.answer;
+                vm.imgSrc = response.data.image;
+            }).catch(function (error) {
+                vm.msg = 'Error! Could not reach the API. ' + error;
+            });
         }
     }
 </script>
