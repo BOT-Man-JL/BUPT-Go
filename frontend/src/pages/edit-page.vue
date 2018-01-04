ecffcb6cf4ce805030337abb8136aa518ca90d11
Aymdr <759826887@qq.com>
2018-01-04 22:29:32 +0800

    Add edit page

--- frontend/src/pages/edit-page.vue
+++ frontend/src/pages/edit-page.vue
@@ -1,6 +1,34 @@
 <template>
     <div class="hello">
-        <p>article {{ $route.params.articleId }} here</p>
+    <el-col :span="10">
+    	<el-row>
+    		<p>标题:</p>
+    		<el-input 
+            size="small" 
+            	placeholder="请输入内容"
+    		    prefix-icon="el-icon-search"
+    		    v-model="title"
+          	>
+          	</el-input>
+    	</el-row>
+    	<el-row>
+    		<p>时间:</p>
+    		<el-date-picker
+      			v-model="date"
+      			type="date"
+      			placeholder="选择日期">
+    		</el-date-picker>
+    	</el-row>
+    	<el-row>
+    		<p>介绍:</p>
+    		<el-input
+  				type="textarea"
+  				:autosize="{ minRows: 2, maxRows: 6}"
+  				placeholder="请输入内容"
+  				v-model="text">
+			</el-input>
+    	</el-row>
+    </el-col>
     </div>
 </template>
 
@@ -8,7 +36,11 @@
     export default {
         name: 'editPage',
         data() {
-            return null;
+            return{
+            	title:'',
+            	text:'',
+            	date:''
+            };
         }
     }
 </script>
